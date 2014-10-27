116d83042bfc2340730ffdd4b2e5b4dcba0bcef8
Marla Brizel <marlabrizel@gmail.com>
2014-10-27 12:56:07 -0600

    Add string interpolation

--- dog_years.rb
+++ dog_years.rb
@@ -1,4 +1,4 @@
 puts 'Hello there, how old are you?'
 user_age = gets.chomp!
-puts 'You are ' + user_age + ' years old?'
+puts "You are #{user_age} years old?"
 puts 'That would make you ' + (user_age.to_i*7).to_s + ' years old in dog years!'
