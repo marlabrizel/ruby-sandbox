b47c68a6cd25fede2b41ad9bda5c3e60ab1fe5c8
Marla Brizel <marlabrizel@gmail.com>
2014-11-02 11:47:50 -0700

    Add remaining leap year logic

--- leapyear.rb
+++ leapyear.rb
@@ -1,8 +1,8 @@
 puts "Give me a calendar year."
 
-input = gets.chomp
+input = gets.chomp.to_i
 
-if input.to_i%4 == 0
+if input%4 == 0 && !(input%100 == 0 && input%400 != 0)
   puts "That's a leap year!"
 else
   puts "That's not a leap year!"
