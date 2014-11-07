a1db536629e9975f91f2e65f69b82c6c7a0a450b
Marla Brizel <marlabrizel@users.noreply.github.com>
2014-11-07 16:56:06 -0700

    Add spaces for legibility between modulos

--- leapyear.rb
+++ leapyear.rb
@@ -2,7 +2,7 @@ puts "Give me a calendar year."
 
 input = gets.chomp.to_i
 
-if input%4 == 0 && !(input%100 == 0 && input%400 != 0)
+if input % 4 == 0 && !(input % 100 == 0 && input % 400 != 0)
   puts "That's a leap year!"
 else
   puts "That's not a leap year!"
